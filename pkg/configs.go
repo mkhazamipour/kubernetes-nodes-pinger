faedae1dc5248f5117164ceab80b396ba645c107
Morteza Khazamipour <mormoroth@gmail.com>
2023-09-09 19:30:38 +0200

    Add read from kubeconfig file and return a clientset

--- pkg/configs.go
+++ pkg/configs.go
@@ -2,13 +2,34 @@ package pkg
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
+
+type KubernetesClient struct {
+	kubernetesClient *kubernetes.Clientset
+}
 
 func LoadEnvVariables() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+// Read kubeconfig file and return kubernetes clientset
+func (c *KubernetesClient) loadClient() *kubernetes.Clientset {
+	config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG_LOCATION"))
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	c.kubernetesClient = clientset
+	return c.kubernetesClient
 }
