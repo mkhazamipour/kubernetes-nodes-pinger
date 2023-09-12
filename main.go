d24928d29d3c59b759e217f456b2abc35feeca5c
Morteza Khazamipour <mormoroth@gmail.com>
2023-09-12 11:53:42 +0200

    add prometehus server

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/mkhazamipour/kubernetes-network-functionality-check/pkg"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func init() {
 	pkg.LoadEnvVariables()
 }
 func main() {
-
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 	kubeClient := pkg.KubernetesClient{}
 	kubeClient.PingNodes()
 }
